9f01fd40ee7d4c599d397f29e7a9d15c969ea250
Ngô Hoàng Anh <ngohoanganh@NgAnh.local>
2023-02-20 14:17:50 +0700

    push code result

--- answer_3/src/Comment_screen.tsx
+++ answer_3/src/Comment_screen.tsx
@@ -1,11 +1,11 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import data from './data.json';
-
+import moment from 'moment';
 interface dataComment {
   id: number;
   content: string;
-  createdAt: number;
+  createdAt: number | string;
   score: number;
   username: string;
   replyingTo?: number;
@@ -17,48 +17,83 @@ const Comment_screen = () => {
 
   // resolve data
   useEffect(() => {
-    const dataInput: dataComment[] = data;
-    const initArr: dataComment = data[0];
-    initArr.childCmt = [] as dataComment[];
-    const arrResolve: dataComment[] = [JSON.parse(JSON.stringify(initArr))];
-
-    // const dataChild_1 = dataInput.filter(item => item.replyingTo);
-    
-    // for(let i = 0; i < dataChild_1.length; i++){
-    //   for(let j = 1; j < dataChild_1.length; j++){
-    //     if(dataChild_1[i].)
-    //   }
-    // }
-
-    // for (let i = 1; i < dataInput.length; i++) {
-    //   if (dataInput[i].replyingTo) {
-    //     for (let j = 0; j < arrResolve.length; j++) {
-    //       if (dataInput[i].replyingTo === arrResolve[j].id) {
-    //         arrResolve[j].childCmt?.push(dataInput[i]);
-    //       } else if (arrResolve[j].childCmt?.length !== 0) {
-    //         // loop resolve
-    //         const childData: dataComment[] = arrResolve[j].childCmt;
-    //         const dataPush: dataComment = dataInput[i];
-    //       }
-    //     }
-    //   } else {
-    //     arrResolve.push({...dataInput[i], childCmt: []});
-    //   }
-    // }
-    setDataAfterResolve(arrResolve);
+    const dataInput: dataComment[] = data.map((item, _) => {
+      return {
+        ...item,
+        childCmt: [] as dataComment[],
+      };
+    });
+    // sort array
+    for (let i = 0; i < dataInput.length; i++) {
+      for (let j = i; j < dataInput.length; j++) {
+        if (dataInput[i].createdAt < dataInput[j].createdAt) {
+          let temp = dataInput[i];
+          dataInput[i] = dataInput[j];
+          dataInput[j] = temp;
+        }
+      }
+    }
+    // push data
+    for (let i = 0; i < dataInput.length; i++) {
+      for (let j = 0; j < dataInput.length; j++) {
+        if (dataInput[i].id === dataInput[j].replyingTo) {
+          dataInput[i].childCmt?.push(dataInput[j]);
+        }
+      }
+    }
+    const finalData = dataInput.filter(item => item.replyingTo === undefined);
+    setDataAfterResolve(finalData);
   }, []);
+
+  const ViewComment = (dataCmt: dataComment) => {
+    return (
+      <View style={styles.boxCmt}>
+        <Text>NAME: {dataCmt.username}</Text>
+        <Text>CONTENT: {dataCmt.content}</Text>
+        <Text>
+          CREATE AT: {moment(dataCmt.createdAt).format('DD/MM/YYYY hh:mm:ss')}
+        </Text>
+        <View style={styles.viewScore}>
+          <Text>Like: {dataCmt.score}</Text>
+          <Text>Cmt: {dataCmt.childCmt?.length}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  const _renderItemChild = (_item: dataComment[], _idx: number): any => {
+    return (
+      <>
+        {_item.map((_itemChild: dataComment, _idxChild: number) => {
+          return (
+            <View
+              key={`${_idx}_${_itemChild.username}`}
+              style={styles.marginLeft}>
+              {ViewComment(_itemChild)}
+              {_itemChild.childCmt && _itemChild.childCmt?.length !== 0 && (
+                <>{_renderItemChild(_itemChild.childCmt, _idxChild)}</>
+              )}
+            </View>
+          );
+        })}
+      </>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      {dataAfterResolve.map((item, index) => {
-        return (
-          <View key={index}>
-            {item.replyingTo}
-            <Text>{item.username}</Text>
-            <Text>{item.username}</Text>
-          </View>
-        );
-      })}
+      <ScrollView>
+        {dataAfterResolve.map((item, index) => {
+          return (
+            <View key={`${index}_${item.username}`}>
+              {ViewComment(item)}
+              {item.childCmt && item.childCmt?.length !== 0 && (
+                <>{_renderItemChild(item.childCmt, index)}</>
+              )}
+            </View>
+          );
+        })}
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -68,5 +103,28 @@ export default Comment_screen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 10,
+  },
+  boxCmt: {
+    padding: 15,
+    borderWidth: 1,
+    marginBottom: 15,
+    borderRadius: 10,
+    borderColor: '#ccc',
+    zIndex: 100,
+  },
+  marginLeft: {marginLeft: 40},
+  viewScore: {
+    position: 'absolute',
+    paddingVertical: 2,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    right: 10,
+    bottom: -10,
+    borderColor: '#ccc',
+    minWidth: 150,
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
   },
 });
